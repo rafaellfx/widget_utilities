e20e8aa0387a5f6cee450ed61e39131bfa20196a
Rafael Lima Felix <rafaellfx@gmail.com>
2025-10-10 11:00:57 -0300

    first commit

---
+++ example/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:widget_utilities/widget_utilities.dart';
+
+void mai() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Widget Utilities')),
+        body: RefreshUniversal(
+          child: Container(),
+          onRefresh: () async {
+            print('Refresh!');
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/refresh_universal.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class RefreshUniversal extends StatelessWidget {
+  final Widget child;
+  final Future<void> Function() onRefresh;
+
+  const RefreshUniversal({
+    super.key,
+    required this.child,
+    required this.onRefresh,
+  });
+
+  bool _isScrollableWidget(Widget widget) {
+    // Verifica se o widget é um tipo que naturalmente é scrollável
+    return widget is ScrollView ||
+        widget is ListView ||
+        widget is GridView ||
+        widget is CustomScrollView ||
+        widget is SingleChildScrollView ||
+        widget is PageView ||
+        widget is NestedScrollView;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Caso o filho já seja um widget com scroll, apenas adicionamos o RefreshIndicator
+    if (_isScrollableWidget(child)) {
+      return RefreshIndicator(onRefresh: onRefresh, child: child);
+    }
+
+    // Caso não seja, envolvemos com SingleChildScrollView e AlwaysScrollableScrollPhysics
+    return LayoutBuilder(
+      builder: (_, constrains) {
+        return RefreshIndicator(
+          onRefresh: onRefresh,
+          child: ScrollConfiguration(
+            behavior: _NoGlowBehavior(),
+            child: SingleChildScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constrains.maxHeight,
+                  minWidth: constrains.maxWidth,
+                ),
+                child: child,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _NoGlowBehavior extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+    BuildContext context,
+    Widget child,
+    ScrollableDetails details,
+  ) {
+    // Remove o efeito de overscroll (glow azul padrão)
+    return child;
+  }
+}
